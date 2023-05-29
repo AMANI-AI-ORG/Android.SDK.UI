8bfec6f56a86ff6f14f801a289d3005d1c5dad50
zekierciyas <zeki@amani.tech>
2023-05-29 16:02:43 +0300

    [ADD] Sample app to demonstrate UI module

---
+++ app/src/main/kotlin/ai/amani/sampleamaniui2/KeyboardExt.kt
@@ -0,0 +1,33 @@
+package ai.amani.sampleamaniui2
+
+import android.content.Context
+import android.content.Intent
+import android.os.Build.VERSION.SDK_INT
+import android.os.Bundle
+import android.os.Parcelable
+import android.view.inputmethod.InputMethodManager
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+
+
+/**
+ * @Author: zekiamani
+ * @Date: 14.03.2023
+ */
+
+fun AppCompatActivity.hideKeyboard() {
+    this.currentFocus?.let {
+        val inputMethodManager = ContextCompat.getSystemService(this, InputMethodManager::class.java)!!
+        inputMethodManager.hideSoftInputFromWindow(it.windowToken, 0)
+    }
+}
+
+inline fun <reified T : Parcelable> Intent.parcelable(key: String): T? = when {
+    SDK_INT >= 33 -> getParcelableExtra(key, T::class.java)
+    else -> @Suppress("DEPRECATION") getParcelableExtra(key) as? T
+}
+
+inline fun <reified T : Parcelable> Bundle.parcelable(key: String): T? = when {
+    SDK_INT >= 33 -> getParcelable(key, T::class.java)
+    else -> @Suppress("DEPRECATION") getParcelable(key) as? T
+}
---
+++ app/src/main/kotlin/ai/amani/sampleamaniui2/MainRegisterActivity.kt
@@ -0,0 +1,78 @@
+package ai.amani.sampleamaniui2
+
+import AmaniV1UI
+import ai.amani.base.utility.AmaniVersion
+import ai.amani.sdk.model.KYCResult
+import ai.amani.sdk.utils.AppConstant
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ *  Basic Sample Activity to launch amani-sdk module
+ */
+class MainRegisterActivity : AppCompatActivity() {
+
+    private val registerButton: Button by lazy { findViewById(R.id.btn) }
+    private val inputIdLabel: EditText by lazy { findViewById(R.id.input_id_number) }
+    private val inputBirthDateLabel: EditText by lazy { findViewById(R.id.input_birth_date) }
+    private val inputExpiryDateLabel: EditText by lazy { findViewById(R.id.input_expiry_date) }
+    private val inputDocumentNumberLabel: EditText by lazy { findViewById(R.id.input_doc_number) }
+
+
+    private val resultLauncher = this.registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        Toast.makeText(this, "Activity result", Toast.LENGTH_LONG).show()
+
+        if (result.resultCode == Activity.RESULT_OK) {
+            // There are no request codes
+            val data: Intent? = result.data
+            data?.let {
+                val kycResult: KYCResult? = it.parcelable(AppConstant.KYC_RESULT)
+                Toast.makeText(this, "Kyc result: ${kycResult!!.profileStatus}" , Toast.LENGTH_LONG).show()
+            }?: let {
+                Toast.makeText(this, "Data is null", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register_main)
+
+        AmaniV1UI.init(
+            activity = this,
+            serverURL = TestCredentials.SERVER_URL,
+            amaniVersion = AmaniVersion.V2,
+            sharedSecret = null
+        )
+
+        registerButton.setOnClickListener {
+
+            hideKeyboard()
+
+            if (inputIdLabel.text.isNullOrEmpty() && inputIdLabel.text.isNullOrBlank()) {
+                Toast.makeText(this, "Please enter your ID", Toast.LENGTH_LONG).show()
+                return@setOnClickListener
+            }
+
+            AmaniV1UI.goToKycActivity(
+                activity = this,
+                resultLauncher = resultLauncher,
+                idNumber = inputIdLabel.text.toString(),
+                authToken = TestCredentials.TOKEN,
+                language = "tr",
+                geoLocation = true,
+                birthDate = inputBirthDateLabel.text.toString(),
+                expireDate = inputExpiryDateLabel.text.toString(),
+                documentNumber = inputDocumentNumberLabel.text.toString(),
+                userEmail = "",
+                userPhoneNumber = ""
+            )
+        }
+    }
+}
+
